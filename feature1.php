f0f2654bb2e4d8c315872d88c881a869c02e6b27
vei tsi <xtfkpi@gmail.com>
2017-03-09 16:06:15 +0200

    feature1.php added

---
+++ feature1.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of feature1
+ *
+ * @author pydev
+ */
+class feature1 {
+    //put your code here
+}
